c85bbe308b7c6715936958e0c7452ced6b2735d2
Sumit <sumitgarg380@gmail.com>
2023-12-04 18:38:14 +0530

    First Commit

---
+++ components/Categories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Category from './Category'
+
+const Categories = () => {
+  return (
+    <div className='flex gap-6 mb-8'>
+      <div>
+        <Category />
+      </div>
+      <div>
+        <Category />
+      </div>
+      <div>
+        <Category />
+      </div>
+      <div>
+        <Category />
+      </div>
+    </div>
+  )
+}
+
+export default Categories
---
+++ components/Category.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div className='bg-teal-400 p-4 rounded-lg shadow-md cursor-pointer'>
+      Category
+    </div>
+  )
+}
+
+export default Category
